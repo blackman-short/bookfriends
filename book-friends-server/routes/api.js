3dc02da3d4ce57e2a8659b81a543df97d531a172
blackman-short <613222346@qq.com>
2018-04-19 17:04:44 +0800

    Updates apiDocument

--- book-friends-server/routes/api.js
+++ book-friends-server/routes/api.js
@@ -12,7 +12,7 @@ router.use('/user', user)
 router.use('/book', book)
 router.use('/admin', admin)
 router.use('/dynamic', dynamic)
-router.use('./comment', comment)
+router.use('/comment', comment)
 router.use('/friend', userFriend)
 router.use('/userbook', userBook)
 
